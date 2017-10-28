ac693ee313a54f1791210c670b4564c5243ae3cc
DESKTOP-I5FJJPR\Perska <perska124@wp.pl>
2017-10-28 18:04:31 +0200

    Created tornado scee prefab and endGamePanel

---
+++ Assets/Scripts/EndGamePanelScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndGamePanelScript : MonoBehaviour
+{
+   public Text txt_title;
+   public Text txt_content;
+   public Button btn_repeatLevel;
+   public Button btn_mainMenu;
+   public Button btn_nextLevel;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void SetAll(string title, string content) {
+        txt_title.text = title;
+        txt_content.text = content;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/FinishPointScript.cs
+++ Assets/Scripts/FinishPointScript.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class FinishPointScript : MonoBehaviour
 {
-
+    SceneMasterScript sceneMaster;
     // Use this for initialization
     void Start()
     {
-
+        sceneMaster = GameObject.FindObjectOfType<SceneMasterScript>();
     }
 
     // Update is called once per frame
@@ -18,11 +18,12 @@ public class FinishPointScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        sceneMaster.SetConditionsSate(true);
         Debug.Log("Enter house");
     }
 }
