fd9930c70a2e9e5016e067970f5c07536c03250b
DESKTOP-I5FJJPR\Perska <perska124@wp.pl>
2017-10-28 14:36:18 +0200

    Added touch recognition to test

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
-
+    Rigidbody2D rigidbody2d;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        rigidbody2d = this.GetComponent<Rigidbody2D>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+       // rigidbody2d.velocity = new Vector2(0, 0.1f);
+
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+
+    public void voidAddForceLeft() {
+        rigidbody2d.AddForce(new Vector2(100, 0));
+    }
 }
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+    private GameObject player;
+    PlayerScript playerScript;
+    // Use this for initialization
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<PlayerScript>();
+    }
+
+
+  
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.touchCount == 1)
+        {
+            // touch on screen
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Debug.Log("Touch detected");
+                movePlayerLeft();
+
+            }
+        }
+        
+    }
+
+
+    public void movePlayerLeft() {
+        playerScript.voidAddForceLeft();
+    }
+}
