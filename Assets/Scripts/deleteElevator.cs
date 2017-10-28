0bef449e3d51ea30fa1b259f29811aadf9e8a125
DESKTOP-CBEU3SL\Grzegorz <grzp91@gmail.com>
2017-10-28 21:17:33 +0200

    Elevator scene

---
+++ Assets/Scripts/deleteElevator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deleteElevator : MonoBehaviour {
+    
+
+    private void OnMouseDown()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+}
