babd73772de6827571507ca1c58f36aac5867824
DESKTOP-I5FJJPR\Perska <perska124@wp.pl>
2017-10-29 03:57:24 +0100

    Added tap icon

---
+++ Assets/Scripts/DestroyAfterTimeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTimeScript : MonoBehaviour {
+
+    public float destroyTime=0.5f;
+	// Use this for initialization
+	void Start () {
+        Destroy(this.gameObject, destroyTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
