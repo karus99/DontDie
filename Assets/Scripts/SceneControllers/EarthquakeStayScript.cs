0da813f1f46fd565c04ba31ff5658c7a9291b7d5
Karol Drwiła <k.drwila@gmail.com>
2017-11-05 17:30:29 +0100

    Revised EarthquakeStay assets.

---
+++ Assets/Scripts/SceneControllers/EarthquakeStayScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthquakeStayScript: MonoBehaviour
+{
+    private float shakeAmount = 0.1f;
+    Vector3 originalCameraPos;
+    private GameObject mainCamera;
+
+    void Start()
+    {
+        GameObject sceneMaster = GameObject.FindGameObjectWithTag("SceneMaster");
+        sceneMaster.GetComponent<SceneMasterScript>().SetConditionsState(true);
+
+        mainCamera = Camera.main.gameObject;
+        originalCameraPos = mainCamera.transform.localPosition;
+    }
+
+    private void Update()
+    {
+        Vector3 newPosition = originalCameraPos + Random.insideUnitSphere * shakeAmount;
+        newPosition.z = originalCameraPos.z;
+        mainCamera.transform.localPosition = newPosition;
+    }
+
+    private void OnDestroy()
+    {
+        mainCamera.transform.localPosition = originalCameraPos;
+    }
+}
